564599eab27d34a36b939b6b1104dbe73d901003
CainLuo <350116542@qq.com>
2020-09-23 18:38:33 +0800

    Update ScanManager.swift

--- Source/ScanManager.swift
+++ Source/ScanManager.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import AVFoundation
+
+open class ScanManager: NSObject {
+    
+    let device = AVCaptureDevice.default(for: .video)
+    
+    var input: AVCaptureDeviceInput?
+    var output: AVCaptureMetadataOutput?
+
+    let session = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    var stillImageOutput: AVCaptureStillImageOutput?
+    
+    var results: [ScanResultModel] = []
+
+    
+}
+
+// MARK: Scan Control
+extension ScanManager {
+    func start() {
+        if !session.isRunning {
+            
+            session.startRunning()
+        }
+    }
+    
+    func stop() {
+        if session.isRunning {
+            
+            session.stopRunning()
+        }
+    }
+}
+
+// MARK: AVCaptureMetadataOutputObjectsDelegate
+extension ScanManager: AVCaptureMetadataOutputObjectsDelegate {
+    public func metadataOutput(_ output: AVCaptureMetadataOutput,
+                               didOutput metadataObjects: [AVMetadataObject],
+                               from connection: AVCaptureConnection) {
+    }
+}
