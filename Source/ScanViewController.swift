8a17f2b7ba9e069b903728152b0850f7b7350c02
CainLuo <350116542@qq.com>
2020-09-23 18:23:24 +0800

    Create ScanViewController.swift

---
+++ Source/ScanViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ScanViewController.swift
+//  Scan-Swift
+//
+//  Created by YYKJ0048 on 2020/9/23.
+//
+
+import UIKit
+
+class ScanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
