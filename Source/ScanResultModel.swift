ee9acb9c5ee999578e0debcadba8655c4215b2e9
CainLuo <350116542@qq.com>
2020-09-24 10:21:50 +0800

    Update ScanResultModel.swift

--- Source/ScanResultModel.swift
+++ Source/ScanResultModel.swift
@@ -6,15 +6,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 public struct ScanResultModel {
     
     public var content: String?
     public var image: UIImage?
-    public var codeType: String?
+    public var codeType: AVMetadataObject.ObjectType?
     public var scanCorner: [Any]?
     
-    public init(content: String?, image: UIImage?, codeType: String?, scanCorner: [Any]?) {
+    public init(content: String?, image: UIImage?, codeType: AVMetadataObject.ObjectType?, scanCorner: [Any]?) {
         self.content = content
         self.image = image
         self.codeType = codeType
