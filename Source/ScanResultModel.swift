29dc899267ecd7c9cb0557f22ae0acb5f7e6c911
CainLuo <350116542@qq.com>
2020-09-24 20:28:46 +0800

    Update ScanResultModel.swift

--- Source/ScanResultModel.swift
+++ Source/ScanResultModel.swift
@@ -13,10 +13,15 @@ public struct ScanResultModel {
     public var content: String?
     public var image: UIImage?
     public var codeType: AVMetadataObject.ObjectType?
+    public var obj: AVMetadataMachineReadableCodeObject?
+    public var feature: CIQRCodeFeature?
     
-    public init(content: String?, image: UIImage?, codeType: AVMetadataObject.ObjectType?) {
+    public init(content: String?, image: UIImage?, codeType: AVMetadataObject.ObjectType?,
+                obj: AVMetadataMachineReadableCodeObject? = nil, feature: CIQRCodeFeature? = nil) {
         self.content = content
         self.image = image
         self.codeType = codeType
+        self.obj = obj
+        self.feature = feature
     }
 }
