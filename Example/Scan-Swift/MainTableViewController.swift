02a878a5305c081cd37fd5398393e22f6f6c63d0
CainLuo <350116542@qq.com>
2021-02-09 15:58:54 +0800

    Create MainTableViewController.swift

---
+++ Example/Scan-Swift/MainTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTableViewController.swift
+//  Scan-Swift_Example
+//
+//  Created by CainLuo on 2021/2/8.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
